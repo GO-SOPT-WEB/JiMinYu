90106fe1282e67b3438c9ae7e1e4d960240259d9
jimyu <aliyah_7@naver.com>
2023-04-20 23:03:22 +0900

    [Feat] 투두 아이템 배열은 따로 상수로 만들어서 사용하기

---
+++ todo/todolist.js
@@ -0,0 +1,20 @@
+const todoItems = [
+	{
+		title: "Jimyu",
+		contents: ["낮잠", "영화 보기", "방 청소"],
+	},
+	{
+		title: "SOPT",
+		contents: ["2차 과제 제출", "스터디 킥오프", "코드리뷰 남기기"],
+	},
+	{
+		title: "HUFS",
+		contents: ["졸프 개발 진행", "중간고사 공부"],
+	},
+	{
+		title: "운동",
+		contents: ["행사 준비", "숨쉬기 운동"],
+	},
+];
+
+export { todoItems };
