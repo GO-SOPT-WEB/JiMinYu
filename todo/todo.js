1937feaec00c69b088f7fb38ffc70ea9ad1be5b8
jimyu <aliyah_7@naver.com>
2023-04-20 23:51:47 +0900

    [Feat] 상수 인덱스랑 요일 활용해서 캘린더 내용 그리기

--- todo/todo.js
+++ todo/todo.js
@@ -1,16 +1,18 @@
-import { todoItems } from "./todolist";
+// import { todoItems } from "./todolist.js";
 
 const body = document.querySelector("body");
 const main = document.querySelector("main");
+const cardSection = document.querySelector(".weekly-section");
 const days = ["월", "화", "수", "목", "금", "토", "일"];
 
 days.forEach((days, index) => {
-	createCalendar;
+	createCalendar(days, index);
 });
 
-function createCalendar() {
+function createCalendar(days, index) {
 	const weeklyItem = document.createElement("article");
-	main.appendChild(weeklyItem);
+	cardSection.appendChild(weeklyItem);
+	weeklyItem.classList.add("weekly-section__item");
 
 	const weeklyDay = document.createElement("h3");
 	weeklyDay.innerText = `${days}`;
@@ -18,10 +20,11 @@ function createCalendar() {
 	const weeklyImg = document.createElement("img");
 	const weeklyImgNum = document.createElement("p");
 	const weeklyNum = document.createElement("p");
-	weeklyNum.innerText = 27 + `${index}`;
+	weeklyNum.innerText = Number(27) + Number(`${index}`);
+
 	weeklyImg.src = "./images/black-heart.png";
 	weeklyImg.alt = "검정-하트";
 
-	weeklyItem.appendChild(weeklyDay, weeklyImgDiv, weeklyNum);
-	weeklyImgDiv.appendChild(weeklyImg, weeklyImgNum);
+	weeklyItem.append(weeklyDay, weeklyImgDiv, weeklyNum);
+	weeklyImgDiv.append(weeklyImg, weeklyImgNum);
 }
