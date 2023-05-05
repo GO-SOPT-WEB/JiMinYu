0b85d28ba97a9b11f97d961d0e813d176843c6ff
jimyu <aliyah_7@naver.com>
2023-05-05 21:33:16 +0900

    feature: 리셋 버튼 기능 추가

--- cardgame/src/pages/main/MainPage.jsx
+++ cardgame/src/pages/main/MainPage.jsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { useState } from "react";
-import { LevelButtons } from "../../components/Buttons";
+import { LevelButton, ResetButton } from "../../components/Buttons";
 import Header from "../../components/Header";
 import CardSection from "../../components/CardSection";
+import CHUNSIK_DATA from "../../assets/CHUNSIK_DATA";
 
 // 버튼 누르면 난이도에 맞게 카드 개수랑 점수판 세팅
 const MainPage = () => {
@@ -10,6 +11,9 @@ const MainPage = () => {
 	const [level, setLevel] = useState("EASY");
 	const [score, setScore] = useState(0);
 	const [cardNum, setCardNum] = useState(5);
+	const [clickedFirst, setClickedFirst] = useState(null);
+	const [clickedSecond, setClickedSecond] = useState(null);
+	const [isFlipped, setIsFlipped] = useState(false);
 
 	// 선택한 레벨에 맞는 카드 개수 지정
 	level === "easy"
@@ -17,8 +21,31 @@ const MainPage = () => {
 		: level === "normal"
 		? setCardNum(7)
 		: setCardNum(9);
+
 	// 레벨 버튼 누르면 카드가 초기화되어야 함
-	const resetCards = () => {};
+	const resetCards = () => {
+		setScore(0);
+		setClickedFirst(null);
+		setClickedSecond(null);
+		setIsFlipped(false);
+	};
+
+	// 리셋 버튼 누르면 전체 초기화 되어야 함
+	const resetAll = () => {
+		setScore(0);
+		setLevel("EASY");
+		setClickedFirst(null);
+		setClickedSecond(null);
+		setIsFlipped(false);
+	};
+
+	// 클릭시 이미지 띄우기
+	const handleClick = (image) => {
+		clickedFirst ? setClickedSecond(image) : setClickedFirst(image);
+	};
+
+	// 카드 맞는지 검사하기
+
 	return (
 		<>
 			<Header
@@ -27,16 +54,18 @@ const MainPage = () => {
 				setScore={setScore}
 				setCardNum={setCardNum}
 			></Header>
-			<LevelButtons
+			<ResetButton resetAll={resetAll}>RESET</ResetButton>
+			<LevelButton
 				currentLevel={level}
 				setLevel={setLevel}
 				reset={resetCards}
-			></LevelButtons>
+			></LevelButton>
 			<StyledMain>
 				<CardSection
 					numCardItems={cardNum}
 					currentLevel={level}
 					cardItemNums={cardNum * 2}
+					cardData={CHUNSIK_DATA}
 				></CardSection>
 			</StyledMain>
 		</>
