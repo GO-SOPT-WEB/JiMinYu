dca4e2b8c51e19e31bdfcbecb1cf932e6b11c8cc
jimyu <aliyah_7@naver.com>
2023-05-12 16:17:40 +0900

    chores: 불필요한 페이지 삭제

--- week4/weather-forecast/src/Router.jsx
+++ week4/weather-forecast/src/Router.jsx
@@ -1,18 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/Mainpage";
 import WeatherCard from "./components/WeatherCard";
-import WeatherCardList from "./pages/WeatherCardList";
+
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<MainPage />} />
-				<Route path="/day" element={<WeatherCardList />}>
+				<Route path=":period">
 					<Route path=":area" element={<WeatherCard />} />
 				</Route>
-				<Route path="/week" element={<WeatherCardList />}>
-					<Route path=":area" element={<WeatherCard />} />
-				</Route>
+				<Route></Route>
 			</Routes>
 		</BrowserRouter>
 	);
--- week4/weather-forecast/src/pages/WeatherCardList.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet } from "react-router-dom";
-import WeatherCard from "../components/WeatherCard";
-import { WEATHER_TYPE } from "../constants/weather";
-const WeatherCardList = () => {
-	return <Outlet />;
-};
-
-export default WeatherCardList;
