a53d5f55a666f2b935e03c8dad40d4562d551bc9
jimyu <aliyah_7@naver.com>
2023-05-12 15:46:33 +0900

    feature: Route elements 수정

--- week4/weather-forecast/src/Router.jsx
+++ week4/weather-forecast/src/Router.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { MainPage } from "./pages/Mainpage";
-import { Cards, WeekWeatherCards, DayWeatherCards } from "./components";
-
+import MainPage from "./pages/Mainpage";
+import WeatherCard from "./components/WeatherCard";
+import WeatherCardList from "./pages/WeatherCardList";
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<MainPage />} />
-				<Route path="/day" element={<DayWeatherCards />}>
-					<Route path=":area" element={<Cards />} />
+				<Route path="/day" element={<WeatherCardList />}>
+					<Route path=":area" element={<WeatherCard />} />
 				</Route>
-				<Route path="/week" element={<WeekWeatherCards />}>
-					<Route path=":area" element={<Cards />} />
+				<Route path="/week" element={<WeatherCardList />}>
+					<Route path=":area" element={<WeatherCard />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
