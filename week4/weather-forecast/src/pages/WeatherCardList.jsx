8a14b13504d93f53102eed32fbbc6f47bb70a079
jimyu <aliyah_7@naver.com>
2023-05-12 15:59:43 +0900

    chores: 이미지 불러오기

--- week4/weather-forecast/src/pages/WeatherCardList.jsx
+++ week4/weather-forecast/src/pages/WeatherCardList.jsx
@@ -1,6 +1,6 @@
-import PageLayout from "../components/PageLayout";
 import { Outlet } from "react-router-dom";
-
+import WeatherCard from "../components/WeatherCard";
+import { WEATHER_TYPE } from "../constants/weather";
 const WeatherCardList = () => {
 	return <Outlet />;
 };
