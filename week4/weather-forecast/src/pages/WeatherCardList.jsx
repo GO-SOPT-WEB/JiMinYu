f40c7b559efca3a6b702b78129b6715a7c7961fd
jimyu <aliyah_7@naver.com>
2023-05-12 15:47:09 +0900

    feature: 카드 띄우는 페이지 생성

---
+++ week4/weather-forecast/src/pages/WeatherCardList.jsx
@@ -0,0 +1,8 @@
+import PageLayout from "../components/PageLayout";
+import { Outlet } from "react-router-dom";
+
+const WeatherCardList = () => {
+	return <Outlet />;
+};
+
+export default WeatherCardList;
