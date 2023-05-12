de47507fa6a0f4ae5b33f04be2aa34d15e973f3e
jimyu <aliyah_7@naver.com>
2023-05-12 13:42:45 +0900

    style: 색깔 테마 추가

--- week4/weather-forecast/src/styles/theme.js
+++ week4/weather-forecast/src/styles/theme.js
@@ -1,7 +1,11 @@
 import { css } from "styled-components";
 
 const colors = {
-	Sky_Blue: "#FFFFF",
+	Sky_Blue: "#C8F0EE",
+	Light_Yellow: "#FFF8E7",
+	Light_Green: "#BFF5CC",
+	Light_Pink: "#F6B3D3",
+	Light_Gray: "#E5E4E2",
 };
 
 const fonts = {};
