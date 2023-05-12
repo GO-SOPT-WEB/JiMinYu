9a3feb46ab6a71da6fcd4d251b16e7c137eb3277
jimyu <aliyah_7@naver.com>
2023-05-12 15:47:35 +0900

    feature: 메인페이지에 카드 리스트 띄우는 화면 추가

--- week4/weather-forecast/src/pages/Mainpage.jsx
+++ week4/weather-forecast/src/pages/Mainpage.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { PageLayout } from "../components/PageLayout";
+import PageLayout from "../components/PageLayout";
+import WeatherCardList from "./WeatherCardList";
 
 const Mainpage = () => {
 	return (
 		<PageLayout>
+			<WeatherCardList />
 			<Outlet />
 		</PageLayout>
 	);
