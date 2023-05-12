eb79da367ee7f7d82bf6348bfd18a0fe74ec0394
jimyu <aliyah_7@naver.com>
2023-05-12 21:15:46 +0900

    feature: 가져온 데이터 카드에 넣어서 띄워주기

--- week4/weather-forecast/src/components/DayWeatherCard.jsx
+++ week4/weather-forecast/src/components/DayWeatherCard.jsx
@@ -1,31 +1,45 @@
 import { styled } from "styled-components";
 import PageLayout from "./PageLayout";
+import { useParams } from "react-router-dom";
+import { useDayWeatherInfo } from "../api/useWeatherInfo";
+import { useEffect } from "react";
+import WEATHER_TYPE from "../constants/weather";
 
 const DayWeatherCard = () => {
+	const area = useParams();
+	const dayWeatherInfo = useDayWeatherInfo(area);
+
+	//사진 로딩
+	useEffect(() => {
+		const weatherImage = WEATHER_TYPE.filter(
+			(images) => images.description === dayWeatherInfo.weather[0].description
+		)[0].imgURL;
+	}, [dayWeatherInfo]);
+
 	return (
 		<PageLayout>
 			<CardStyle>
 				<header>
-					<h3>${}</h3>
+					<h3>{dayWeatherInfo.name}</h3>
 				</header>
-				<img src="" alt="${area}-사진" />
+				<img src={weatherImage} alt={dayWeatherInfo.weather.description} />
 				<div>
 					<span>온도</span>
-					<span>${}°C</span>
+					<span>{dayWeatherInfo.main.temp}°C</span>
 				</div>
 				<div>
 					<span>체감 온도</span>
-					<span>${}°C</span>
+					<span>{dayWeatherInfo.main.feels_like}°C</span>
 				</div>
 				<div>
 					<span>최저/최고</span>
 					<span>
-						${}°C/${}°C
+						{dayWeatherInfo.main.temp_min}°C/{dayWeatherInfo.main.temp_max}°C
 					</span>
 				</div>
 				<div>
 					<span>구름</span>
-					<span>${}%</span>
+					<span>{dayWeatherInfo.clouds.all}%</span>
 				</div>
 			</CardStyle>
 		</PageLayout>
