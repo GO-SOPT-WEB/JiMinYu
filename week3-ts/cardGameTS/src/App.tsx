3e015bf5c2cf9b5bb7313646c2e51b952d157255
jimyu <aliyah_7@naver.com>
2023-06-09 21:36:24 +0900

    init

--- week3-ts/cardGameTS/src/App.tsx
+++ week3-ts/cardGameTS/src/App.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles";
+import GlobalStyle from "./styles/GlobalStyle";
 import { theme } from "./styles/theme";
+import { ThemeProvider } from "styled-components";
 
 function App() {
 	return (
-		<>
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
 			<BrowserRouter>
 				<Routes>
 					<Route></Route>
 				</Routes>
 			</BrowserRouter>
-			<GlobalStyles />
-		</>
+		</ThemeProvider>
 	);
 }
 
