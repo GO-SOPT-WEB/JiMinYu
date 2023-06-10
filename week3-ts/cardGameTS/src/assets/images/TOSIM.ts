3df375351b1bd74e49b5cfb2e26795a78ee783b0
jimyu <aliyah_7@naver.com>
2023-06-10 19:04:36 +0900

    feat: 이미지 파일에 isCorrect, isFlipped 추가

--- week3-ts/cardGameTS/src/assets/images/TOSIM.ts
+++ week3-ts/cardGameTS/src/assets/images/TOSIM.ts
@@ -4,60 +4,80 @@ const TOSIM = [
 		alt: "사랑-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/3247f87329f1a29e692a5f3e39c2c82382f3bd8c9735553d03f6f982e10ebe70",
+		isFlipped: false,
+		isCorrect: false,
 	},
 	{
 		id: 2,
 		alt: "조아합니다-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/3247f87329f1a29e692a5f3e39c2c8234022de826f725e10df604bf1b9725cfd",
+		isFlipped: false,
+		isCorrect: false,
 	},
 	{
 		id: 3,
 		alt: "리듬-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/3247f87329f1a29e692a5f3e39c2c82341d1a2caccd0c566eab28b91e2e5d306",
+		isFlipped: false,
+		isCorrect: false,
 	},
 	{
 		id: 4,
 		alt: "볼살-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/9c67797af35b26ee079c69b8d3c748648f324a0b9c48f77dbce3a43bd11ce785",
+		isFlipped: false,
+		isCorrect: false,
 	},
 	{
 		id: 5,
 		alt: "하트-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/9c67797af35b26ee079c69b8d3c748649f17e489affba0627eb1eb39695f93dd",
+		isFlipped: false,
+		isCorrect: false,
 	},
 	{
 		id: 6,
 		alt: "열창-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/9c67797af35b26ee079c69b8d3c748644022de826f725e10df604bf1b9725cfd",
+		isFlipped: false,
+		isCorrect: false,
 	},
 	{
 		id: 7,
 		alt: "응원-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/9c67797af35b26ee079c69b8d3c74864a88f7b2cbb72be0bdfff91ad65b168ab",
+		isFlipped: false,
+		isCorrect: false,
 	},
 	{
 		id: 8,
 		alt: "치킨-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/9c67797af35b26ee079c69b8d3c74864ac8e738cb631e72fdb9a96b36413984e",
+		isFlipped: false,
+		isCorrect: false,
 	},
 	{
 		id: 9,
 		alt: "휴식-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/9c67797af35b26ee079c69b8d3c74864960f4ab09fe6e38bae8c63030c9b37f9",
+		isFlipped: false,
+		isCorrect: false,
 	},
 	{
 		id: 10,
 		alt: "뽀뽀-토심",
 		imgSrc:
 			"https://item.kakaocdn.net/do/9c67797af35b26ee079c69b8d3c74864effd194bae87d73dd00522794070855d",
+		isFlipped: false,
+		isCorrect: false,
 	},
 ];
 
