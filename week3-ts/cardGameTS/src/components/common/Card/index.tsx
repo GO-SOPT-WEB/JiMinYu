fdf7c20632b356a5d0a00ad0094153a6f34dc6c6
jimyu <aliyah_7@naver.com>
2023-06-10 23:46:50 +0900

    feat: 클릭시 카드 뒤집어지도록 수정중
    - 리렌더링 문제 발생

--- week3-ts/cardGameTS/src/components/common/Card/index.tsx
+++ week3-ts/cardGameTS/src/components/common/Card/index.tsx
@@ -2,7 +2,7 @@ import Button from "../Button";
 import * as Styled from "./style";
 import { tosim_background } from "../../../assets/images";
 import TOSIM from "../../../assets/images/TOSIM";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { LevelState, ScoreState } from "../../../recoil/atom";
 
@@ -14,9 +14,9 @@ const HARD = 18;
 interface CardItemProps {
 	id: number;
 	alt: string;
-	imgSrc: string;
-	isCorrect: boolean;
-	isFlipped: boolean;
+	src: string;
+	correct: boolean;
+	flipped: boolean;
 }
 
 //카드 랜덤으로 섞기
@@ -28,43 +28,77 @@ const shuffleCards = (cardList: CardItemProps[]) => {
 	}
 };
 
-//섞은 카드 중에서 레벨에 맞는 개수만큼 잘라주고 만들기
-const createCards = (level: number) => {
-	const levelCardList = TOSIM.slice(0, level);
-
-	return levelCardList.map((id) => (
-		<Styled.CardItem isCorrect={id.isCorrect} isFlipped={id.isFlipped}>
-			{id.isFlipped ? (
-				<Styled.CardImgBack
-					className="cardBack"
-					src={tosim_background}
-					alt="토심카드-뒷면"
-				/>
-			) : (
-				<Styled.CardImgFront
-					className="cardFront"
-					src={id.imgSrc}
-					alt={id.alt}
-				/>
-			)}
-		</Styled.CardItem>
-	));
-};
-
 //카드 두 개 눌렸을 때마다 정답여부 판단하는 useEffect 함수
 
 export const Card = () => {
 	const [level, setLevel] = useRecoilState(LevelState);
 	const [score, setScore] = useRecoilState(ScoreState);
 
+	const [cardList, setCardList] = useState<CardItemProps[]>(TOSIM);
+	const [flippedCards, setFlippedCards] = useState<number[]>([]);
+
 	const [isReset, setIsReset] = useState(false);
+
+	const toggleCardFlip = (card: CardItemProps) => {
+		if (flippedCards.length === 2) {
+			return;
+		}
+
+		// 	const updatedCard = { ...card, flipped: !card.flipped };
+		// 	if (!card.flipped && !card.correct) {
+		// 		return { ...card, flipped: true };
+		// 	}
+		// 	setCardList([])
+	};
+
+	//섞은 카드 중에서 레벨에 맞는 개수만큼 잘라주고 만들기
+	const createCards = (level: number) => {
+		let newCardList = TOSIM.slice(0, level / 2);
+		newCardList = [...newCardList, ...newCardList];
+		setCardList(newCardList);
+		shuffleCards(cardList);
+
+		return cardList.map((card, index) => (
+			<Styled.CardItem
+				key={index}
+				correct={card.correct ? 1 : 0}
+				flipped={card.flipped ? 1 : 0}
+			>
+				{card.flipped ? (
+					<Styled.CardImgFront
+						className="cardFront flipped"
+						src={card.src}
+						alt={card.alt}
+						onClick={() => toggleCardFlip(card)}
+					/>
+				) : (
+					<Styled.CardImgBack
+						className="cardBack"
+						src={tosim_background}
+						alt="토심카드-뒷면"
+						onClick={() => {
+							toggleCardFlip(card);
+						}}
+					/>
+				)}
+			</Styled.CardItem>
+		));
+	};
 
 	const onLevelClicked = (cardNums: number) => {
 		setLevel(cardNums);
 		setIsReset(true);
 		shuffleCards(TOSIM);
 	};
+
 	//리셋 처리해주는 useEffect(setIsReset 거짓으로 바꿔놔야 함)
+	useEffect(() => {
+		setLevel(level);
+	}, [level]);
+
+	useEffect(() => {
+		setIsReset(false);
+	}, [isReset]);
 
 	return (
 		<Styled.CardSection>
