a573991f3bc08917cbdcca53bc7c93904293dd8a
jimyu <aliyah_7@naver.com>
2023-06-10 11:16:52 +0900

    feat: added Header and Card in mainpage

--- week3-ts/cardGameTS/src/pages/mainpage.tsx
+++ week3-ts/cardGameTS/src/pages/mainpage.tsx
@@ -1,10 +1,11 @@
+import { Card } from "../components/common/Card";
 import { Header } from "../components/common/Header";
 
 const MainPage = () => {
 	return (
 		<>
 			<Header />
-			<div>안녕</div>
+			<Card />
 		</>
 	);
 };
