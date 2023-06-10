0d129baac11021c7bc11cc8e22528fc4ccb2b66d
jimyu <aliyah_7@naver.com>
2023-06-10 19:03:12 +0900

    feat: using atoms in Header

--- week3-ts/cardGameTS/src/components/common/Header/index.tsx
+++ week3-ts/cardGameTS/src/components/common/Header/index.tsx
@@ -1,12 +1,19 @@
+import { useRecoilState } from "recoil";
+import { LevelState, ScoreState } from "../../../recoil/atom";
 import Button from "../Button";
 import * as Styled from "./style";
 
 export const Header = () => {
+	const [level, setLevel] = useRecoilState(LevelState);
+	const [score, setScore] = useRecoilState(ScoreState);
+
 	return (
 		<Styled.Root>
 			<h1>🐰토심아 어딨니?!🐰</h1>
 			<Styled.Div>
-				<span>0/5</span>
+				<span>
+					{score}/{level / 2}
+				</span>
 				<Button>다시 할래!</Button>
 			</Styled.Div>
 		</Styled.Root>
